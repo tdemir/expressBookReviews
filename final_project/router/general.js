10c69f0dfc0a9c2c4e60c21d850f67676c0fa00d
tdemir <tdemir@users.noreply.github.com>
2024-07-08 13:54:51 +0300

    task-2 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,8 +18,18 @@ public_users.get("/", function (req, res) {
 
 // Get book details based on ISBN
 public_users.get("/isbn/:isbn", function (req, res) {
+  const isbn = req.params.isbn;
+  if (isbn) {
+    console.log(books.length);
+    let bookItem = books[isbn];
+    if (bookItem) {
+      res.send(JSON.stringify(bookItem));
+    }
+    res.status(400).send("item doesnot exist");
+  }
+  res.status(400).send("isbn doesnot send");
   //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  //return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get book details based on author
