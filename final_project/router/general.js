6b683224156b9fb265e46f3ed96e18ae9a95dd35
tdemir <tdemir@users.noreply.github.com>
2024-07-08 14:09:47 +0300

    task-5 done

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,14 +20,13 @@ public_users.get("/", function (req, res) {
 public_users.get("/isbn/:isbn", function (req, res) {
   const isbn = req.params.isbn;
   if (isbn) {
-    console.log(books.length);
     let bookItem = books[isbn];
     if (bookItem) {
-      res.send(JSON.stringify(bookItem));
+      return res.send(JSON.stringify(bookItem));
     }
-    res.status(400).send("item doesnot exist");
+    return res.status(400).send("item doesnot exist");
   }
-  res.status(400).send("isbn parameter doesnot send");
+  return res.status(400).send("isbn parameter doesnot send");
   //Write your code here
   //return res.status(300).json({ message: "Yet to be implemented" });
 });
@@ -74,8 +73,16 @@ public_users.get("/title/:title", function (req, res) {
 
 //  Get book review
 public_users.get("/review/:isbn", function (req, res) {
-  //Write your code here
-  return res.status(300).json({ message: "Yet to be implemented" });
+  const isbn = req.params.isbn;
+  if (isbn) {
+    let bookItem = books[isbn];
+    if (bookItem) {
+      return res.send(JSON.stringify(bookItem.reviews));
+    }
+    return res.status(400).send("item doesnot exist");
+  }
+  return res.status(400).send("isbn parameter doesnot send");
+  //return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 module.exports.general = public_users;
